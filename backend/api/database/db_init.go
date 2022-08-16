4aed553e81ee4e004be518cb73d74aed7c732eda
pipe-dealer <batteredandspicy@gmail.com>
2022-08-17 00:01:00 +0900

    new file:   backend/postgres.env     - env. file containing  credentials required to make connection to Postgres database. Will be encrypted in the future for security
        new file:   backend/api/database/db_init.go
        - connects to Postgres database

---
+++ backend/api/database/db_init.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+// variable that points to the postgres connection, allows web server to interact with database
+var Db *sql.DB
+
+func init() {
+	//get db credentials from env. file
+	err := godotenv.Load("postgres.env")
+	if err != nil {
+		panic(err)
+	}
+
+	var (
+		host     = os.Getenv("host")
+		port, _  = strconv.Atoi(os.Getenv("port"))
+		user     = os.Getenv("user")
+		password = os.Getenv("password")
+		dbname   = os.Getenv("dbname")
+	)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	//connect to db server
+	Db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+}
