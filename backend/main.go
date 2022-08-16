65ad08aeefeb1c210102514f61aa541cf9219455
pipe-dealer <batteredandspicy@gmail.com>
2022-08-16 17:09:38 +0900

    Testing server

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "Hello world",
+		})
+	})
+
+	r.Run("192.168.0.8:8080")
+}
