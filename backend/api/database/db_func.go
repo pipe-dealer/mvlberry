4df8094197da7bab15768f7646bb5b46d2413db5
pipe-dealer <batteredandspicy@gmail.com>
2022-08-17 22:56:17 +0900

    modified:   backend/api/database/db_func.go     improved error handling and fixed mistakes

--- backend/api/database/db_func.go
+++ backend/api/database/db_func.go
@@ -1,18 +1,20 @@
 package database
 
+import "fmt"
+
 //User datatype
 type User struct {
-	username string `json:"username"`
-	password string `json:"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 //checks if the new username already exists
-func checkDuplicates(username string) bool {
+func checkDuplicates(username string) int {
 
-	sqlSelect := "SELECT username from users"
+	sqlSelect := "SELECT username FROM users"
 	rows, err := Db.Query(sqlSelect)
 	if err != nil {
-		panic(err)
+		return 2
 	}
 
 	//iterates through every record and checks if username is the same as new username
@@ -22,30 +24,31 @@ func checkDuplicates(username string) bool {
 			panic(err)
 		}
 		if currentusername == username {
-			return true
+			return 1
 		}
 	}
 
-	return false
+	return 0
 }
 
 //Adds new user to database, and returns a response code, which will produce a different response message depending on the code
 func Adduser(newuser User) int {
-	username := newuser.username
-	password := newuser.password
+	username := newuser.Username
+	password := newuser.Password
 
 	//if username does not exist, add new user to database
-	if !checkDuplicates(username) {
+	if checkDuplicates(username) == 0 {
 		sqlInsert := "INSERT INTO users (username,password) VALUES ($1,$2)"
 
 		if _, err := Db.Exec(sqlInsert, username, password); err != nil {
 			panic(err)
 		}
-
+		fmt.Println(username)
 		return 0 //user successfully created
-	} else if checkDuplicates(username) {
+	} else if checkDuplicates(username) == 1 {
 		return 1 //username already exists
+	} else {
+		return 2 //could not create user
 	}
 
-	return 2 //could not create user
 }
