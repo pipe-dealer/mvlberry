71154a1e250c378e26d73809d7efb4171c1f3f95
pipe-dealer <batteredandspicy@gmail.com>
2022-08-18 23:51:26 +0900

    new file:   frontend/src/App.js     Main file where other components are rendered

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SignupForm from './Components/SignupForm';
+
+class App extends Component {
+    render() {
+        return (
+            <SignupForm />
+        )
+    }
+}
+
+export default App
