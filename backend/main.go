21e082e876fa7b0607f4b1c694dff9977762a3f4
pipe-dealer <batteredandspicy@gmail.com>
2022-08-18 23:49:48 +0900

    modified:   backend/main.go     Added CORS support so that request could be made between frontend and backend

--- backend/main.go
+++ backend/main.go
@@ -5,8 +5,25 @@ import (
 	"github.com/mvlberry/backend/api/handlers"
 )
 
+func setCORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
+	r.Use(setCORS())
 
 	//if /api/signup is hit with a POST request, run the signup function
 	r.POST("/api/signup", handlers.Signup)
