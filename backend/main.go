6e6b471bccbb26d3d4fb3f62004417399f625a15
pipe-dealer <batteredandspicy@gmail.com>
2022-08-19 22:55:35 +0900

    modified:   backend/main.go     test handler for login

--- backend/main.go
+++ backend/main.go
@@ -27,6 +27,7 @@ func main() {
 
 	//if /api/signup is hit with a POST request, run the signup function
 	r.POST("/api/signup", handlers.Signup)
+	r.GET("/api/login", handlers.Login)
 
 	r.Run("localhost:8080")
 }
