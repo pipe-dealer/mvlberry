032292644d381860b086ad923d0c9ab7fe873f74
pipe-dealer <batteredandspicy@gmail.com>
2022-08-19 21:25:30 +0900

    new file:   frontend/src/Components/LoginPage.js     Simple login page

---
+++ frontend/src/Components/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+class LoginPage extends Component {
+    render() {
+        return(
+            <h1>
+                Login
+            </h1>
+        )
+    }
+}
+
+export default LoginPage
