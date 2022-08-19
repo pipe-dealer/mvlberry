230c3c42b22e7d66164899bf1224747707ed15db
pipe-dealer <batteredandspicy@gmail.com>
2022-08-19 21:25:17 +0900

    new file:   frontend/src/Components/Home.js     Simple homepage

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+class Home extends Component {
+    render() {
+        return(
+            <h1>
+                Home
+            </h1>
+        )
+    }
+}
+
+export default Home
