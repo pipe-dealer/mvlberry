24a35ce2e951fd71da75374d7d2ca5235d0a20ce
pipe-dealer <batteredandspicy@gmail.com>
2022-08-20 00:29:36 +0900

    modified:   frontend/src/Components/Home.js     Created simple homepage that displays current username if logged in

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
-class Home extends Component {
-    render() {
+const Home = () => {
+    const cookies = new Cookies();
+    //check if user has logged in
+    if (cookies.get('loggedin') == 1) {
+        //displays a welcome message with username
         return(
+            <div>
+                <h1>
+                    Welcome {cookies.get('currentuser')}
+                </h1>
+            </div>
+        )
+    } else {
+        return (
             <h1>
                 Home
             </h1>
