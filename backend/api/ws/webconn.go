7dc0c0de20f872614a0413827c18dfc065039ff8
pipe-dealer <batteredandspicy@gmail.com>
2022-08-23 00:06:57 +0900

    modified:   backend/api/ws/webconn.go     extended test endpoint by adding channels, where clients would be added to certain channels based on a counter

--- backend/api/ws/webconn.go
+++ backend/api/ws/webconn.go
@@ -8,36 +8,58 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// upgrader variable, which will be used to upgrade connection to websocket
 var Upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true }, //allow any connection connect to websocket enpoint
 }
 
-func Reader(conn *websocket.Conn) {
-	for {
-		// get message sent from client
-		msgType, msg, err := conn.ReadMessage()
-		if err != nil {
-			panic(err)
-		}
+var count = 0
 
-		fmt.Println("Message sent was: ", string(msg))
-		//send it back to the client
-		if err := conn.WriteMessage(msgType, msg); err != nil {
-			panic(err)
+// adds client to channel if it exists, otherwise create a new channel
+func serve(count int, ws *websocket.Conn) {
+	var chatchannel *ChatChannel
+	f := false
+	//using a simple counter as channel ID, if channel ID exists, add client to that channel
+	for _, v := range Allchannels {
+		if count == v.Id {
+			chatchannel = v
+			f = true
+			break
 		}
 	}
+	//otherwise create a new channel with counter as channel ID
+	if !f {
+		chatchannel = Newchannel(count)
+	}
+	go chatchannel.Start()
+
+	client := Client{
+		Conn:        ws,
+		ChatChannel: chatchannel,
+	}
+
+	fmt.Println("Number of chats:", len(Allchannels))
+
+	chatchannel.Joined <- &client
+	client.Read()
+
 }
 
 func Startws(c *gin.Context) {
-	//upgrade connection to websocket
 	ws, err := Upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		c.JSON(http.StatusBadGateway, gin.H{
 			"msg": "Could not establish connection",
 		})
 	}
-	Reader(ws)
+	//counter will alternate between 1 and 2, which determines which channel new clients will go into
+	count += 1
+	if count > 2 {
+		count = 1
+	}
+
+	serve(count, ws)
 
 }
