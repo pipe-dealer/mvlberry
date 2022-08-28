5dedca561115cd15c26a315dc33473971b00f3ff
pipe-dealer <batteredandspicy@gmail.com>
2022-08-29 00:11:17 +0900

    modified:   backend/api/ws/client.go     includes the time the message was sent in message body

--- backend/api/ws/client.go
+++ backend/api/ws/client.go
@@ -1,26 +1,30 @@
 package ws
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gorilla/websocket"
 )
 
 // Client datatype, contains information about user, websocket connection and channel it is connected to
 type Client struct {
-	// Id      int
-	Conn        *websocket.Conn
-	ChatChannel *ChatChannel
+	Id       int             //client id
+	Username string          //client username
+	Conn     *websocket.Conn //pointer to websocket connection
+	Chats    *Chat           //current chat it is connected to
 }
 
 // message datatype
 type Message struct {
-	MessageType int
-	Msg         []byte
+	MessageType int    //this will typically be 1
+	Msg         []byte //message is sent as bytes
 }
 
 // client function that runs until client closes the websocket connection
 func (c *Client) Read() {
 	defer func() {
-		c.ChatChannel.Left <- c
+		c.Chats.Left <- c
 		c.Conn.Close()
 	}()
 
@@ -28,13 +32,16 @@ func (c *Client) Read() {
 		//get message sent from client
 		msgType, msg, err := c.Conn.ReadMessage()
 		if err != nil {
-			panic(err)
+			fmt.Println("client.go 34")
 		}
+
+		currentTime := time.Now()
+
 		message := Message{
 			MessageType: msgType,
-			Msg:         msg,
+			Msg:         []byte(fmt.Sprintf("[%v]	%v: %v", currentTime.Format(time.ANSIC), c.Username, string(msg))),
 		}
 		//passes it to send channel, where message will be processed and sent to all other clients
-		c.ChatChannel.Send <- message
+		c.Chats.Send <- message
 	}
 }
