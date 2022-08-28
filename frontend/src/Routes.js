a77bf27a37a7af3e269d5d357078b862f78df3f4
pipe-dealer <batteredandspicy@gmail.com>
2022-08-29 00:13:57 +0900

    modified:   frontend/src/Routes.js     route path for chat page

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import SignupPage from "./Components/SignupPage";
 import Home from "./Components/Home"
 import LoginPage from "./Components/LoginPage"
+import Chat from "./Components/Chat";
+
 
 const RouteHandler = () => {
     return (
@@ -10,7 +12,7 @@ const RouteHandler = () => {
             <Route path='/' element={<Home/>}></Route>
             <Route path='/signup' element={<SignupPage/>}></Route>
             <Route path="/login" element={<LoginPage/>}></Route>
-
+            <Route path="/chat" element={<Chat/>}></Route>
         </Routes>
     )
 }
