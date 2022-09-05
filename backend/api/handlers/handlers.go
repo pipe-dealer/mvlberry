4ea5bcf5b3b66e4149ccd4978de40c47406a6917
pipe-dealer <batteredandspicy@gmail.com>
2022-09-05 15:00:58 +0900

    modified:   backend/api/handlers/handlers.go     handler functions for sending and getting friend requests

--- backend/api/handlers/handlers.go
+++ backend/api/handlers/handlers.go
@@ -22,21 +22,19 @@ func Signup(c *gin.Context) {
 
 	//checks if account could be successfully added to database
 	switch res := database.Adduser(newUser); res {
-	case 0:
+	case 0: //signup successful
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "Account successfully created. Redirecting to login page",
 		})
 		fmt.Printf("User %s was successfully created\n", newUser.Username)
-	case 1:
+	case 1: //username exists
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "Username already exists",
 		})
-		fmt.Println("Could not create account: username already exists")
-	case 2:
+	case 2: //overall error
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "Could not create account",
 		})
-		fmt.Println("Could not create account")
 
 	}
 }
@@ -51,7 +49,6 @@ func Login(c *gin.Context) {
 	}
 	//get all users
 	allusers := database.Getusers()
-
 	for _, v := range allusers {
 		c_username, c_password := v.Username, v.Password
 
@@ -64,7 +61,7 @@ func Login(c *gin.Context) {
 				})
 				fmt.Printf("User %s has logged in\n", user.Username)
 				return
-			} else {
+			} else { //incorrect password
 				c.JSON(http.StatusOK, gin.H{
 					"msg": "Username or password incorrect",
 				})
@@ -73,39 +70,38 @@ func Login(c *gin.Context) {
 			}
 		}
 	}
-
+	//incorrect username
 	c.JSON(http.StatusOK, gin.H{
 		"msg": "Username or password incorrect",
 	})
-
-	fmt.Println("A user has tried to login in with an unknown account")
-
 }
 
-func Getfriends(c *gin.Context) {
+func GetFriends(c *gin.Context) {
 	var allfriends []string
-	username := c.Query("user")
+	username := c.Query("current_user")
 	friends := database.Getfriends(username)
-	fmt.Println("Getting friends of user ", username)
+
 	//get friend's username add it to array
 	for _, v := range friends {
-		f_user := database.GetuserByID(v.F_id).Username
-		allfriends = append(allfriends, f_user)
+		f_user, _ := database.GetuserbyID(v.F_id)
+		f_username := f_user.Username
+		allfriends = append(allfriends, f_username)
 	}
 	//send friends' usernames back to client
 	c.JSON(http.StatusOK, allfriends)
-
 }
 
-func Startws(c *gin.Context) {
-	// client starts ws connection with another friend, sends query with their username and friend's username
+func StartWS(c *gin.Context) {
 	var chatId int
-
+	// client starts ws connection with another friend, sends query with their username and friend's username
 	user1 := c.Query("user1")
 	user2 := c.Query("user2")
 
-	user1_id := database.GetuserByUsername(user1).Id
-	user2_id := database.GetuserByUsername(user2).Id
+	//get users' id
+	user1_details, _ := database.Getuserbyusername(user1)
+	user2_details, _ := database.Getuserbyusername(user2)
+	user1_id := user1_details.Id
+	user2_id := user2_details.Id
 
 	user1_friends := database.Getfriends(user1)
 
@@ -118,4 +114,68 @@ func Startws(c *gin.Context) {
 	}
 	//initiate websocket connection
 	ws.Startws(user1, user1_id, chatId, c)
+}
+
+func SendRequest(c *gin.Context) {
+	//get current user and requested user
+	var newRequest database.Request
+	if err := c.ShouldBindJSON(&newRequest); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": "Could not parse JSON",
+		})
+
+	}
+	c_user, _ := database.Getuserbyusername(newRequest.C_username)
+	c_id := c_user.Id
+
+	//check if requested user exists
+	req_user, err := database.Getuserbyusername(newRequest.Req_username)
+	if err == 1 {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": fmt.Sprintf("%v is not a valid username.", newRequest.Req_username),
+		})
+		return
+	}
+	req_id := req_user.Id
+
+	//check if user is already friend of current user
+	for _, v := range database.Getfriends(newRequest.C_username) {
+		if v.F_id == req_id {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": fmt.Sprintf("%v is already a friend", newRequest.Req_username),
+			})
+			return
+		}
+	}
+
+	if res := database.Addrequest(c_id, req_id); res == 1 {
+		c.JSON(http.StatusOK, gin.H{ //general error
+			"msg": "Could not send request",
+		})
+	} else if res == 0 { //successful request
+		c.JSON(http.StatusOK, gin.H{
+			"msg": fmt.Sprintf("Request sent to %v", newRequest.Req_username),
+		})
+		fmt.Printf("%v sent a request to %v \n", newRequest.C_username, newRequest.Req_username)
+	} else if res == 2 { //request already sent or received from same user
+		c.JSON(http.StatusOK, gin.H{
+			"msg": fmt.Sprintf("You have already sent a request to %v or %v has sent a request to you", newRequest.Req_username, newRequest.Req_username),
+		})
+
+	}
+
+}
+
+func GetIncomingRequests(c *gin.Context) {
+	var requester_usernames []string
+	c_username := c.Query("current_user")
+	c_user, _ := database.Getuserbyusername(c_username)
+	c_id := c_user.Id
+	allRequests := database.Getincomingrequests(c_id)
+
+	for _, v := range allRequests {
+		requester_usernames = append(requester_usernames, v.C_username)
+	}
+	c.JSON(http.StatusOK, requester_usernames)
+
 }
