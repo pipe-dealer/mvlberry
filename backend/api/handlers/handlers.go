836a815db41606756ecdc857daccf5ae5f2292c9
pipe-dealer <batteredandspicy@gmail.com>
2022-09-11 12:21:48 +0900

    modified:   backend/api/handlers/handlers.go     added handler functions for sending and handling friend requests

--- backend/api/handlers/handlers.go
+++ backend/api/handlers/handlers.go
@@ -39,6 +39,7 @@ func Signup(c *gin.Context) {
 	}
 }
 
+// login handler
 func Login(c *gin.Context) {
 	var user database.User
 	//get login details
@@ -47,7 +48,7 @@ func Login(c *gin.Context) {
 			"msg": "Could not parse JSON",
 		})
 	}
-	//get all users
+	//get all users and checks if entered details exist
 	allusers := database.Getusers()
 	for _, v := range allusers {
 		c_username, c_password := v.Username, v.Password
@@ -76,6 +77,7 @@ func Login(c *gin.Context) {
 	})
 }
 
+// get friends of user
 func GetFriends(c *gin.Context) {
 	var allfriends []string
 	username := c.Query("current_user")
@@ -91,6 +93,7 @@ func GetFriends(c *gin.Context) {
 	c.JSON(http.StatusOK, allfriends)
 }
 
+// upgrade connection to websocket and start chat
 func StartWS(c *gin.Context) {
 	var chatId int
 	// client starts ws connection with another friend, sends query with their username and friend's username
@@ -116,9 +119,10 @@ func StartWS(c *gin.Context) {
 	ws.Startws(user1, user1_id, chatId, c)
 }
 
+// send friend request to requested user
 func SendRequest(c *gin.Context) {
 	//get current user and requested user
-	var newRequest database.Request
+	var newRequest database.ReceivedRequest
 	if err := c.ShouldBindJSON(&newRequest); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"msg": "Could not parse JSON",
@@ -161,21 +165,54 @@ func SendRequest(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"msg": fmt.Sprintf("You have already sent a request to %v or %v has sent a request to you", newRequest.Req_username, newRequest.Req_username),
 		})
-
 	}
-
 }
 
+// get friend requests sent to user
 func GetIncomingRequests(c *gin.Context) {
-	var requester_usernames []string
 	c_username := c.Query("current_user")
 	c_user, _ := database.Getuserbyusername(c_username)
 	c_id := c_user.Id
 	allRequests := database.Getincomingrequests(c_id)
 
-	for _, v := range allRequests {
-		requester_usernames = append(requester_usernames, v.C_username)
-	}
-	c.JSON(http.StatusOK, requester_usernames)
+	c.JSON(http.StatusOK, allRequests)
+}
 
+// accept or delete request
+func HandleRequest(c *gin.Context) {
+	//parse JSON data
+	var requestform database.RequestForm
+	if err := c.ShouldBindJSON(&requestform); err != nil {
+		fmt.Println("Could not parse request form")
+	}
+
+	//if user decided to accept request, add
+	if requestform.Accepted {
+		request := database.GetrequestbyID(requestform.Req_id) //get request record
+		switch err := database.Addfriend(request.Req_id, request.Id, request.R_id); err {
+		case 0: //friend request was successfully accepted
+			friend_username, _ := database.GetuserbyID(request.R_id)
+			c.JSON(http.StatusOK, gin.H{
+				"msg": fmt.Sprintf("%v has been added as a friend", friend_username.Username),
+			})
+		case 1: //error
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "Could not add friend",
+			})
+
+		}
+		//if user rejected request
+	} else if !requestform.Accepted {
+		//delete request record
+		if err := database.Deleterequest(requestform.Req_id); err == 1 {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "Could not delete request",
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "Request deleted",
+			})
+
+		}
+	}
 }
